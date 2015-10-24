e66318fb23a871359bee54ee1eff7582e38af083
coreytegeler <coreytegeler@gmail.com>
2015-10-24 11:00:47 -0400

    logging

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log(req);
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -51,7 +52,6 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  console.log('env = development');
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -64,6 +64,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  console.log(req);
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
